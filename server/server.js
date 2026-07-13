156d08b6543d536c465178c2f2b192417eb30059
ussocbot <chantedperez@gmail.com>
2026-07-12 22:30:36 -0500

    Set up Express server with a basic route

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
+import express from "express";
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "BOT SOP API Online"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
