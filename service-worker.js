b5992b2e030c7fe29966eacd390048775f7e10b4
ussocbot <chantedperez@gmail.com>
2026-07-21 12:58:07 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = "bot-sop-static-v18-14";
+const CACHE_NAME = "bot-sop-static-v18-15";
 const STATIC_ASSETS = [
   "/",
   "/index.html",
   "/signed-out.html",
-  "/css/styles.css?v=2026.07.21-v18.14",
-  "/js/data.js?v=2026.07.21-v18.14",
-  "/js/components.js?v=2026.07.21-v18.14",
-  "/js/navigation.js?v=2026.07.21-v18.14",
-  "/js/reviews.js?v=2026.07.21-v18.14",
-  "/js/submissions.js?v=2026.07.21-v18.14",
-  "/js/app.js?v=2026.07.21-v18.14",
+  "/css/styles.css?v=2026.07.21-v18.15",
+  "/js/data.js?v=2026.07.21-v18.15",
+  "/js/components.js?v=2026.07.21-v18.15",
+  "/js/navigation.js?v=2026.07.21-v18.15",
+  "/js/reviews.js?v=2026.07.21-v18.15",
+  "/js/submissions.js?v=2026.07.21-v18.15",
+  "/js/app.js?v=2026.07.21-v18.15",
   "/vendor/lucide.min.js?v=0.468.0",
   "/assets/gear-favicon.svg"
 ];
